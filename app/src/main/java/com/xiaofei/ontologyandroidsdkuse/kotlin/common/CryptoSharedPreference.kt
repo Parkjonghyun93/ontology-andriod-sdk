66275746665a4435abf8a756c4fc64bfcbc1d36d
Jonghyun <kltb930906@gmail.com>
2020-05-18 18:22:23 +0900

    Fix CryptoSharedPreference

--- app/src/main/java/com/xiaofei/ontologyandroidsdkuse/kotlin/common/CryptoSharedPreference.kt
+++ app/src/main/java/com/xiaofei/ontologyandroidsdkuse/kotlin/common/CryptoSharedPreference.kt
@@ -1,6 +1,7 @@
 package com.xiaofei.ontologyandroidsdkuse.kotlin.common
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.xiaofei.ontologyandroidsdkuse.kotlin.resource.OntologyResource
 
 /**
@@ -9,16 +10,19 @@ import com.xiaofei.ontologyandroidsdkuse.kotlin.resource.OntologyResource
  */
 
 object CryptoSharedPreference {
-    private lateinit var context: Context
-
     private const val KEY_ENCRYPTED_MNEMONIC = "KEY_ENCRYPTED_MNEMONIC"
+    private const val KEY_ONTOLOGY_ADDRESS = "KEY_ONTOLOGY_ADDRESS"
+    private const val KEY_ONTOLOGY_KEYSTORE_FILE_DIR = "KEY_ONTOLOGY_KEYSTORE_FILE_DIR"
+
+    private lateinit var context: Context
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedPreferencesEditor: SharedPreferences.Editor
 
     public fun init(context: Context) {
-        CryptoSharedPreference.context = context
+        this.context = context
+        sharedPreferences = context.getSharedPreferences(OntologyResource.KEYSTORE_DIRECTORY_NAME, Context.MODE_PRIVATE)
+        sharedPreferencesEditor = sharedPreferences.edit()
     }
-
-    private val sharedPreferences = context.getSharedPreferences(OntologyResource.KEYSTORE_DIRECTORY_NAME, Context.MODE_PRIVATE)
-    private val sharedPreferencesEditor = sharedPreferences.edit()
 
     fun getEncryptedMnemonic(): String? {
         return sharedPreferences.getString(KEY_ENCRYPTED_MNEMONIC, null)
@@ -28,4 +32,23 @@ object CryptoSharedPreference {
         sharedPreferencesEditor.putString(KEY_ENCRYPTED_MNEMONIC, encryptedMnemonic)
         sharedPreferencesEditor.commit()
     }
+
+    fun getOntologyAddress(): String? {
+        return sharedPreferences.getString(KEY_ONTOLOGY_ADDRESS, null)
+    }
+
+    fun setOntologyAddress(address: String) {
+        sharedPreferencesEditor.putString(KEY_ONTOLOGY_ADDRESS, address)
+        sharedPreferencesEditor.commit()
+    }
+
+    fun getOntologyKeyStoreFileDir(): String? {
+        return sharedPreferences.getString(KEY_ONTOLOGY_KEYSTORE_FILE_DIR, null)
+    }
+
+    fun setOntologyKeyStoreFileDir(fileDir: String) {
+        sharedPreferencesEditor.putString(KEY_ONTOLOGY_KEYSTORE_FILE_DIR, fileDir)
+        sharedPreferencesEditor.commit()
+    }
+
 }
